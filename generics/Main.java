1ddc8677e7c810da1f27bdb7cd3b9fc595cbb3da
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-10-14 19:29:35 +0530

    Added:Functionality to check generic method

--- generics/Main.java
+++ generics/Main.java
@@ -3,7 +3,9 @@ package generics;
 public class Main {
 	public static void main(String[] args) {
 		MaximumOfThree maxOfThree = new MaximumOfThree();
-		String max = maxOfThree.maximum("Apple","Mango","Banana");
-		System.out.println("maximum value:" + max);
+
+		System.out.println("maximum Integer value:" + maxOfThree.maximum(82, 44, 66));
+		System.out.println("maximum Float value:" + maxOfThree.maximum(72.55, 44.33, 66.43));
+		System.out.println("maximum String value:" + maxOfThree.maximum("Apple", "Mango", "Banana"));
 	}
 }
--- generics/MaximumOfThree.java
+++ generics/MaximumOfThree.java
@@ -1,10 +1,10 @@
 package generics;
 
-public class MaximumOfThree {
-	String x, y, z;
+public class MaximumOfThree<T extends Comparable<T>> {
+	T x, y, z;
 
-	public String maximum(String x, String y, String z) {
-		String value = x;
+	public T maximum(T x, T y, T z) {
+		T value = x;
 		if (y.compareTo(value) > 0)
 			value = y;
 		if (z.compareTo(value) > 0)
