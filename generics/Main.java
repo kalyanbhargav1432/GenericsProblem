5c98f6379e9dbe85b2ef37a2aec446d5e10004d4
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-10-14 21:59:23 +0530

    Added:Functionality to create generic class

--- generics/Main.java
+++ generics/Main.java
@@ -2,10 +2,12 @@ package generics;
 
 public class Main {
 	public static void main(String[] args) {
-		MaximumOfThree maxOfThree = new MaximumOfThree();
+		MaximumOfThree maxOfThree1 = new MaximumOfThree(82, 44, 66);
+		MaximumOfThree maxOfThree2 = new MaximumOfThree(82.55, 44.53, 66.35);
+		MaximumOfThree maxOfThree3 = new MaximumOfThree("Apple", "Mango", "Banana");
 
-		System.out.println("maximum Integer value:" + maxOfThree.maximum(82, 44, 66));
-		System.out.println("maximum Float value:" + maxOfThree.maximum(72.55, 44.33, 66.43));
-		System.out.println("maximum String value:" + maxOfThree.maximum("Apple", "Mango", "Banana"));
+		System.out.println("maximum Integer value:" + maxOfThree1.testMaximum());
+		System.out.println("maximum Float value:" + maxOfThree2.testMaximum());
+		System.out.println("maximum String value:" + maxOfThree3.testMaximum());
 	}
 }
--- generics/MaximumOfThree.java
+++ generics/MaximumOfThree.java
@@ -3,7 +3,17 @@ package generics;
 public class MaximumOfThree<T extends Comparable<T>> {
 	T x, y, z;
 
-	public T maximum(T x, T y, T z) {
+	public MaximumOfThree(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public T testMaximum() {
+		return MaximumOfThree.testMaximum(x, y, z);
+	}
+
+	public static <T extends Comparable<T>> T testMaximum(T x, T y, T z) {
 		T value = x;
 		if (y.compareTo(value) > 0)
 			value = y;
