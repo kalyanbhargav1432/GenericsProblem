fe6b82a0f6e7162729aeab9d6f18d1a04ce4f2a0
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-10-15 07:58:23 +0530

    Added:Functionality to add the printmax method

--- generics/Main.java
+++ generics/Main.java
@@ -2,9 +2,9 @@ package generics;
 
 public class Main {
 	public static void main(String[] args) {
-		Integer[] arrayInt = { 12, 34, 5 };
-		Float[] arrayFloat = { 22.34F, 34.55F, 65.64F };
-		String[] arrayString = { "Apple", "Banana", "Mango" };
+		Integer[] arrayInt = { 12, 64, 5,44 };
+		Float[] arrayFloat = { 92.94F, 34.55F, 65.64F,74.32F };
+		String[] arrayString = { "Apple", "Banana", "Mango","Orange" };
 
 		new MaximumOfThree<Integer>(arrayInt).testMaximum();
 		new MaximumOfThree<Float>(arrayFloat).testMaximum();
--- generics/MaximumOfThree.java
+++ generics/MaximumOfThree.java
@@ -11,6 +11,10 @@ public class MaximumOfThree<T extends Comparable<T>> {
 		return MaximumOfThree.testMaximum(array);
 	}
 
+	public static <T> void printMax(T value) {
+		System.out.println("maximum value :" + value);
+	}
+
 	public static <T extends Comparable<T>> T testMaximum(T[] array) {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = i; j < array.length; j++) {
@@ -21,7 +25,7 @@ public class MaximumOfThree<T extends Comparable<T>> {
 				}
 			}
 		}
-		System.out.println("maximum:" + array[0]);
+		MaximumOfThree.printMax(array[0]);
 		return array[0];
 	}
 }
